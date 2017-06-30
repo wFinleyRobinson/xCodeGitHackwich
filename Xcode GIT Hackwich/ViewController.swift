e0cd2593c8ef97e7aa7ab18e51ac6a34be30cfce
William F. Robinson <williamfrobinson@me-2.local>
2017-06-30 10:58:58 -0500

    Added button to view controller

--- Xcode GIT Hackwich/ViewController.swift
+++ Xcode GIT Hackwich/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func onButtonTapped(_ sender: Any) {
+        print("button was tapped")
+    }
 
 
 }
